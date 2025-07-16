47a52bb22f019ef47967c95eb7b95eec7f59e404
Aaron <a@aaronfisher.net>
2025-07-16 17:08:48 +0100

    Updated with new signals

--- td_listener.py
+++ td_listener.py
@@ -12,52 +12,127 @@ mqttc.loop_start()
 
 AREA = 'KG'
 
+# SIGNALS_TO_WATCH = {
+# 	'0078' : {
+# 		'dir' : 'South'
+# 	},
+# 	'3952' : {
+# 		'dir' : 'South'
+# 	},
+# 	'3953' : {
+# 		'dir' : 'North'
+# 	},
+# 	'3954' : {
+# 		'dir' : 'South'
+# 	},
+# 	'3955' : {
+# 		'dir' : 'North'
+# 	},
+# 	'3957' : {
+# 		'dir' : 'North'
+# 	},
+# 	'3958' : {
+# 		'dir' : 'South'
+# 	},
+# 	'3959' : {
+# 		'dir' : 'North'
+# 	},
+# 	'3973' : {
+# 		'dir' : 'North'
+# 	},
+# 	'3975' : {
+# 		'dir' : 'North'
+# 	},
+# }
+# NORTH_OD = OrderedDict()
+# NORTH_OD = {
+# 	'3953' : [],
+# 	'3955' : [],
+# 	'3957' : [],
+# 	'3959' : []
+# }
+# SOUTH_OD = OrderedDict()
+# SOUTH_OD = {
+# 	'3952' : [],
+# 	'3954' : [],
+# 	'3958' : []
+# }
+
 SIGNALS_TO_WATCH = {
-	'0078' : {
+	'0074' : { # Not used
 		'dir' : 'South'
 	},
-	'3952' : {
+	'0076' : { # Not used
 		'dir' : 'South'
 	},
-	'3953' : {
+	'0191' : {
 		'dir' : 'North'
 	},
-	'3954' : {
+	'0192' : {
 		'dir' : 'South'
 	},
-	'3955' : {
+	'0193' : {
 		'dir' : 'North'
 	},
-	'3957' : {
-		'dir' : 'North'
-	},
-	'3958' : {
+	'0194' : {
 		'dir' : 'South'
 	},
-	'3959' : {
+	'0195' : {
 		'dir' : 'North'
 	},
-	'3973' : {
+	'0196' : {
+		'dir' : 'South'
+	},
+	'0197' : {
 		'dir' : 'North'
 	},
-	'3975' : {
+	'0198' : {
+		'dir' : 'South'
+	},
+	'0201' : {
+		'dir' : 'North'
+	},
+	'0202' : {
+		'dir' : 'North'
+	},
+	'0203' : {
+		'dir' : 'North'
+	},
+	'0204' : {
+		'dir' : 'North'
+	},
+	'0205' : { # Not used
+		'dir' : 'North'
+	},
+	'0207' : { # Not used
+		'dir' : 'North'
+	},
+	'0209' : { # Not used
 		'dir' : 'North'
 	},
 }
 
-NORTH_OD = OrderedDict()
-NORTH_OD = {
-	'3953' : [],
-	'3955' : [],
-	'3957' : [],
-	'3959' : []
+LINE_1_NORTH_OD = OrderedDict()
+LINE_1_NORTH_OD = {
+	'0191' : [],
+	'0195' : [],
+	'0205' : [] # Temp fix to see if this helps with the issue of a signal being jumped on this line
 }
-
-SOUTH_OD = OrderedDict()
-SOUTH_OD = {
-	'3952' : [],
-	'3954' : [],
-	'3958' : []
+LINE_1_SOUTH_OD = OrderedDict()
+LINE_1_SOUTH_OD = {
+	'0192' : [],
+	'0196' : []
+}
+LINE_2_NORTH_OD = OrderedDict()
+LINE_2_NORTH_OD = {
+	'0193' : [],
+	'0197' : [],
+	'0203' : []
+}
+LINE_2_SOUTH_OD = OrderedDict()
+LINE_2_SOUTH_OD = {
+	'0194' : [],
+	'0198' : []
 }
 
 class Listener(stomp.ConnectionListener):
@@ -93,24 +168,40 @@ class Listener(stomp.ConnectionListener):
 
 							print(signal_info)
 
-							for key, value in NORTH_OD.items():
+							for key, value in LINE_1_NORTH_OD.items():
 								if signal_info['head_code'] in value:
 									value.remove(signal_info['head_code'])
-							for key, value in SOUTH_OD.items():
+							for key, value in LINE_1_SOUTH_OD.items():
+								if signal_info['head_code'] in value:
+									value.remove(signal_info['head_code'])
+							for key, value in LINE_2_NORTH_OD.items():
+								if signal_info['head_code'] in value:
+									value.remove(signal_info['head_code'])
+							for key, value in LINE_2_SOUTH_OD.items():
 								if signal_info['head_code'] in value:
 									value.remove(signal_info['head_code'])
 							
-							if signal_info['from'] != None and signal_info['from'] in NORTH_OD:
-								NORTH_OD[signal_info['from']].append(signal_info['head_code'])
-							if signal_info['to'] != None and signal_info['to'] in NORTH_OD:
-								NORTH_OD[signal_info['to']].append(signal_info['head_code'])
-							if signal_info['from'] != None and signal_info['from'] in SOUTH_OD:
-								SOUTH_OD[signal_info['from']].append(signal_info['head_code'])
-							if signal_info['to'] != None and signal_info['to'] in SOUTH_OD:
-								SOUTH_OD[signal_info['to']].append(signal_info['head_code'])
+							if signal_info['from'] != None and signal_info['from'] in LINE_1_NORTH_OD:
+								LINE_1_NORTH_OD[signal_info['from']].append(signal_info['head_code'])
+							if signal_info['to'] != None and signal_info['to'] in LINE_1_NORTH_OD:
+								LINE_1_NORTH_OD[signal_info['to']].append(signal_info['head_code'])
+							if signal_info['from'] != None and signal_info['from'] in LINE_1_SOUTH_OD:
+								LINE_1_SOUTH_OD[signal_info['from']].append(signal_info['head_code'])
+							if signal_info['to'] != None and signal_info['to'] in LINE_1_SOUTH_OD:
+								LINE_1_SOUTH_OD[signal_info['to']].append(signal_info['head_code'])
+							if signal_info['from'] != None and signal_info['from'] in LINE_2_NORTH_OD:
+								LINE_2_NORTH_OD[signal_info['from']].append(signal_info['head_code'])
+							if signal_info['to'] != None and signal_info['to'] in LINE_2_NORTH_OD:
+								LINE_2_NORTH_OD[signal_info['to']].append(signal_info['head_code'])
+							if signal_info['from'] != None and signal_info['from'] in LINE_2_SOUTH_OD:
+								LINE_2_SOUTH_OD[signal_info['from']].append(signal_info['head_code'])
+							if signal_info['to'] != None and signal_info['to'] in LINE_2_SOUTH_OD:
+								LINE_2_SOUTH_OD[signal_info['to']].append(signal_info['head_code'])
 
-							print('North', NORTH_OD)
-							print('South', SOUTH_OD)
+							print('Line 1 North', LINE_1_NORTH_OD)
+							print('Line 1 South', LINE_1_SOUTH_OD)
+							print('Line 2 North', LINE_2_NORTH_OD)
+							print('Line 2 South', LINE_2_SOUTH_OD)
 
 							# mqttc.publish('trains/signals', json.dumps(signal_info))
 
@@ -122,5 +213,7 @@ conn.subscribe(destination=f'/topic/TD_ALL_SIG_AREA', id=1, ack='auto')
 while True:
 	time.sleep(5)
 
-	mqttc.publish('trains/signals/north', json.dumps(NORTH_OD))
-	mqttc.publish('trains/signals/south', json.dumps(SOUTH_OD))
+	# mqttc.publish('trains/signals/line-1/north', json.dumps(LINE_1_NORTH_OD))
+	# mqttc.publish('trains/signals/line-1/south', json.dumps(LINE_1_SOUTH_OD))
+	# mqttc.publish('trains/signals/line-2/north', json.dumps(LINE_2_NORTH_OD))
+	# mqttc.publish('trains/signals/line-2/south', json.dumps(LINE_2_SOUTH_OD))
